d92714f35fea1d55eb39eb732cf12cd55a0a3333
Adrian Ruben Dogar <adrian.dogar@gmail.com>
2024-11-08 00:34:57 +0100

    Initial commit - upgrade of the token manager library

--- globals.py
+++ globals.py
@@ -10,3 +10,14 @@ def unset_verbose() -> None:
 
 def get_verbose() -> bool:
     return _verbose
+
+_tokens = {}
+
+def append_token(instance: str, interface: str, token: dict) -> None:
+    global _tokens
+    _tokens.update({instance: {interface: token}})
+
+def get_token(instance: str, interface: str) -> str:
+    global _tokens
+    return _tokens[instance][interface]
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import globals
 
 from args import home as args_handler
@@ -5,10 +7,12 @@ from controllers import commands
 from logger import setup_logger, set_log_level
 
 # import sys
-# from configobj.config import Config
+from configobj.config import Config
+from light_token_manager import LightTokenManager
+
+logger = setup_logger(__name__, "INFO")
 
 def main():
-    logger = setup_logger(__name__, "INFO")
 
     args = args_handler.parse_args()
 
@@ -16,12 +20,38 @@ def main():
         globals.set_verbose()
         set_log_level(logger, "DEBUG")
 
+    # Read instances config
+    config = Config("config.yaml", env_file=".env").items()
+    # print(json.dumps(config, indent=2))
+
+    refresh_tokens(config)
+
     commands.run(args)
 
 
-    # Read instances config
-    # config_parser = Config("instances.json", env_file=".env")
-    # instances_config = config_parser.items()
+def refresh_tokens(config):
+    for instance, instance_details in config['instances'].items():
+        for interface, interface_details in instance_details.items():
+            if interface_details.get('enabled'):
+                logger.debug(f"Fetching token for [{interface}] interface of the [{instance}] instance")
+                ltm = LightTokenManager(
+                    token_url=f"{interface_details['url']}/api/token",
+                    payload_format="json",
+                    body={
+                        "client_id": interface_details['client_id'],
+                        "client_secret": interface_details['client_secret'],
+                        "grant_type": interface_details['grant_type'],
+                        "username": interface_details['username'],
+                        "password": interface_details['password'],
+                        "realm": interface_details['realm']
+                    }
+                )
+                globals.append_token(
+                    instance=instance,
+                    interface=interface,
+                    token=ltm.get_token()
+                )
+
 
 if __name__ == "__main__":
     main()
